39baa2215ac14d56d5487217bc7112882e5034a7
jsh990620 <jsh90620@gmail.com>
2023-03-23 22:59:56 +0900

    name

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -3,11 +3,11 @@ import { Background, LoadingText } from "./Styles";
 import Spinner from "./assets/Spinner.gif";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default function Loading() {
   return (
     <Background>
       <LoadingText>잠시만 기다려 주세요.</LoadingText>
       <img src={Spinner} alt="로딩중" width="5%" />
     </Background>
   );
-};
+}
